8249a8e2408fbdfc274e6081ead870ccd5231230
Sean <one70815@gmail.com>
2022-10-15 01:33:51 +0800

    test2-1st

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
     int x = 10;
     int y = 20;
     int z = 30;
-    printf("x + y + z = %d", x + y + z);
-    printf("Hello World!!!!");
+    printf("x - y + z = %d", x - y + z);
+    printf("Hello World and Goodbye World!!!!");
     return 0;
 }
